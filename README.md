1524218bc6a666056d122b29932bedeb6ceaff8d
FesalBadday <fesal.n04@hotmail.com>
2021-11-02 09:16:53 -0600

    tweaks

---
+++ README.md
@@ -0,0 +1,31 @@
+# CPNT 262 Assignment 4 - Node command line app
+
+
+## Information 
+
+- Did you ever get bored with the black/white color node gives you? Well now you can bring node to life with the [colors](https://www.npmjs.com/package/colors) npm package, not only that but also you can stylize your fonts as well. 
+
+
+## Third-party npm Package
+
+- [colors](https://www.npmjs.com/package/colors)
+
+
+## Three example commands
+
+```js
+node app yellow bold
+```
+
+```js
+node app cyan inverse
+```
+
+```js
+node app magenta strikethrough
+```
+
+
+## Attributions
+
+[colors](https://www.npmjs.com/package/colors)
--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 const colors = require('colors');
 
-const fontColor = ['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'gray', 'grey'];
-const fontStyle = ['reset', 'bold', 'dim', 'italic', 'underline', 'inverse', 'hidden', 'strikethrough'];
+const fontColor = ['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'gray', 'grey']; // array of font colors
+const fontStyle = ['reset', 'bold', 'dim', 'italic', 'underline', 'inverse', 'hidden', 'strikethrough']; // array of font styles
 
-const args = process.argv.slice(2);
+const args = process.argv.slice(2); // get command line input and slice it
 
-const color = args[0];
-const style = args[1];
+const color = args[0]; // get first element of argument
+const style = args[1]; // get second element of argument 
 
-if (!fontColor.includes(args[0])) { // check if color is in the fontColor array
+if (!fontColor.includes(color)) { // check if color is in the fontColor array
 
-  console.log('**Please type a valid color**'.red.inverse); // if not show an error
+  console.log('**Please type a valid color**'.red.inverse.italic); // if not show an error
 
-} else if (!fontStyle.includes(args[1])) { // check if style is in the fontStyle array
+} else if (!fontStyle.includes(style)) { // check if style is in the fontStyle array
 
-  console.log('**Please type a valid style**'.red.inverse); // if not show an error
+  console.log('**Please type a valid style**'.red.inverse.italic); // if not show an error
 
 } else { // else show results
   console.log('\n######################'.rainbow.bold)
 
-  console.log(colors[color][style]('Hello Colourful World!')); // print results
+  console.log(colors[color][style]('Hello Colorful World!')); // print results
 
   console.log('######################\n'.rainbow.bold)
 }
